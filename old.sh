39025f1e9785c5ba8d93b658fa1863eb7a89bf19
Peter Cotton <57455669+microprediction@users.noreply.github.com>
2020-06-16 21:16:11 -0400

    Update old.sh

--- old.sh
+++ old.sh
@@ -3,4 +3,4 @@ cd mining_working_dir
 python3 -m venv miningenv
 source miningenv/bin/activate
 pip install --upgrade microprediction
-python3 -c "import microprediction;microprediction.donate(difficulty=13,password='5581cee8a281f4fd8cbe404143017046',donor='big rig')"
+python3 -c "import microprediction;microprediction.donate(difficulty=13,password='5581cee8a281f4fd8cbe404143017046',donor='pure grunt')"
