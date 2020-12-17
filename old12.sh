d07b539bf11bece6eac299c71f0885e3c7b32fa0
Peter Cotton <57455669+microprediction@users.noreply.github.com>
2020-12-17 09:49:57 -0500

    Create old12.sh

---
+++ old12.sh
@@ -0,0 +1,6 @@
+mkdir mining_working_dir
+cd mining_working_dir
+python3 -m venv miningenv
+source miningenv/bin/activate
+pip install --upgrade microprediction
+python3 -c "import microprediction;microprediction.donate(difficulty=12,password='5581cee8a281f4fd8cbe404143017046',donor='pedal harder')"
