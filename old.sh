9b36bb7af120de9c49be7fc1c60b4da29e7a537c
Peter Cotton <57455669+microprediction@users.noreply.github.com>
2021-11-01 19:38:59 -0400

    Update old.sh

--- old.sh
+++ old.sh
@@ -3,6 +3,7 @@ cd mining_working_dir
 python3 -m venv miningenv
 source miningenv/bin/activate
 pip install --upgrade pip
+pip install --upgrade cython
 pip install --upgrade pytz
 pip install --upgrade git+https://github.com/microprediction/microprediction.git
 python3 -c "import microprediction;microprediction.donate(difficulty=13,password='5581cee8a281f4fd8cbe404143017046',donor='pedal harder')"
