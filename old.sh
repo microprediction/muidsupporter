d14daf3ef44cb09a558bdbce6bd97fb6816b9bf3
Peter Cotton <57455669+microprediction@users.noreply.github.com>
2021-12-26 20:58:49 -0500

    Update old.sh

--- old.sh
+++ old.sh
@@ -4,4 +4,4 @@ python3 -m venv miningenv
 source miningenv/bin/activate
 pip install --upgrade pip
 pip install --upgrade git+https://github.com/microprediction/microconventions.git
-python3 -c "from microconventions.donate import donate;donate(difficulty=13,password='5581cee8a281f4fd8cbe404143017046',donor='downstairs')"
+python3 -c "from microconventions.supporter import donate;donate(difficulty=13,password='5581cee8a281f4fd8cbe404143017046',donor='downstairs')"
